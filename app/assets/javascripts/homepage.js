b1c4ee55deaf25a21c6910c287321b21e50143aa
Alvaro Lobato <alobato678@gmail.com>
2017-11-22 19:09:18 +0000

    Vamos

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,13 +1,4 @@
-// Banner Typed.js
- var options = {
-      strings: ["Hi, I am Álvaro", "I am an Ambitious Programmer", "I am an Ingenious Marketer", "I am a Start-up Builder", "Welcome to my world"],
-      loop: false,
-      typeSpeed: 40,
-      shuffle: false,
-      autoInsertCss: true,
-    }
 
-    var typed = new Typed("#typed", options);
 
 
 // Determine if element is in Viewport!
@@ -46,25 +37,9 @@ function addClassEventListener(element, className) {
 }
 
 
-// Trigger progressBar when shape elements in-view
-function progressBar(selector, shapeVariable, fill) {
-  for (var i = 0; i < selector.length; i++) {
-    if (isAnyPartOfElementInViewport(selector[i])) {
-      shapeVariable.animate(fill);
-    }
-  }
-};
 
 
-// Event Listener to Shape Elements
-function progressBarEventListeners(event, selector, shapeVariable, fill) {
-  window.addEventListener(event, function() {
-    progressBar(selector, shapeVariable, fill);
-  });
-  window.removeEventListener(event, function() {
-    progressBar(selector, shapeVariable, fill);
-  })
-};
+
 
 
 // Define and select class elements
@@ -72,16 +47,10 @@ var timeline = document.querySelectorAll(".timeline li");
 var titles = document.querySelectorAll(".row--left h1")
 
 
-//Define and Select shape elements
-var rubyOnRails = document.querySelectorAll(".card--skill h3");
-var htmlCSS = document.querySelectorAll(".htmlCSS")
 
 
 addClassEventListener(titles, "tracking-in-expand")
 addClassEventListener(timeline, "in-view")
-
-progressBarEventListeners("scroll", rubyOnRails, rorCircle, 0.5)
-progressBarEventListeners(htmlCSS, htmlCircle, 0.5)
 
 
 
